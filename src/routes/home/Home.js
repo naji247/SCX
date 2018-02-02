acaed6e0e345b7a2c330361b52707f8ca8487c73
Derek Lou <dereklou@Dereks-MacBook-Pro.local>
2018-02-01 23:34:14 -0500

    placeholder column names

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -23,38 +23,44 @@ class Home extends React.Component {
   render() {
     return (
       <Table>
-        <TableHeader>
+        <TableHeader displaySelectAll={false}>
           <TableRow>
-            <TableHeaderColumn>ID</TableHeaderColumn>
+            <TableHeaderColumn>Ticker</TableHeaderColumn>
             <TableHeaderColumn>Name</TableHeaderColumn>
-            <TableHeaderColumn>Status</TableHeaderColumn>
+            <TableHeaderColumn>Price</TableHeaderColumn>
+            <TableHeaderColumn>Market Cap</TableHeaderColumn>
+            <TableHeaderColumn>24 Hour Range</TableHeaderColumn>
+            <TableHeaderColumn>3 Month Range</TableHeaderColumn>
+            <TableHeaderColumn>Volatility</TableHeaderColumn>
           </TableRow>
         </TableHeader>
-        <TableBody>
+        <TableBody displayRowCheckbox={false}>
           <TableRow>
-            <TableRowColumn>1</TableRowColumn>
-            <TableRowColumn>John Smith</TableRowColumn>
-            <TableRowColumn>Employed</TableRowColumn>
+            <TableRowColumn>BTC</TableRowColumn>
+            <TableRowColumn>Bitcoin</TableRowColumn>
+            <TableRowColumn>$8,631.01</TableRowColumn>
+            <TableRowColumn>$146B</TableRowColumn>
+            <TableRowColumn>$1,322.32</TableRowColumn>
+            <TableRowColumn>$12,323</TableRowColumn>
+            <TableRowColumn>71%</TableRowColumn>
           </TableRow>
           <TableRow>
-            <TableRowColumn>2</TableRowColumn>
-            <TableRowColumn>Randal White</TableRowColumn>
-            <TableRowColumn>Unemployed</TableRowColumn>
+            <TableRowColumn>BTC</TableRowColumn>
+            <TableRowColumn>Bitcoin</TableRowColumn>
+            <TableRowColumn>$8,631.01</TableRowColumn>
+            <TableRowColumn>$146B</TableRowColumn>
+            <TableRowColumn>1,322.32</TableRowColumn>
+            <TableRowColumn>$12,323</TableRowColumn>
+            <TableRowColumn>71%</TableRowColumn>
           </TableRow>
           <TableRow>
-            <TableRowColumn>3</TableRowColumn>
-            <TableRowColumn>Stephanie Sanders</TableRowColumn>
-            <TableRowColumn>Employed</TableRowColumn>
-          </TableRow>
-          <TableRow>
-            <TableRowColumn>4</TableRowColumn>
-            <TableRowColumn>Steve Brown</TableRowColumn>
-            <TableRowColumn>Employed</TableRowColumn>
-          </TableRow>
-          <TableRow>
-            <TableRowColumn>5</TableRowColumn>
-            <TableRowColumn>Christopher Nolan</TableRowColumn>
-            <TableRowColumn>Unemployed</TableRowColumn>
+            <TableRowColumn>BTC</TableRowColumn>
+            <TableRowColumn>Bitcoin</TableRowColumn>
+            <TableRowColumn>$8,631.01</TableRowColumn>
+            <TableRowColumn>$146B</TableRowColumn>
+            <TableRowColumn>1,322.32</TableRowColumn>
+            <TableRowColumn>$12,323</TableRowColumn>
+            <TableRowColumn>71%</TableRowColumn>
           </TableRow>
         </TableBody>
       </Table>
